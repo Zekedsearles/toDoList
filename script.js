44406159af906a5b1545c00e1b0f2e9d70b8d323
Ezekiel Searles <zekedsearles@gmail.com>
2024-01-11 17:39:31 -0800

    ran into a bug

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-$(document).ready(function(){
-  $.ajax({
-    type: 'GET',
-    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
-    dataType: 'json',
-    success: function (response, textStatus) {
-      response.tasks.forEach(function (task) {
-        $('#todo-list').append('<p>' + task.content + '</p>');
-      })
-    },
-    error: function (request, textStatus, errorMessage) {
-      console.log(errorMessage);
-    }
-  });
-});
-$.ajax({
-    type: 'POST',
-    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1137',
-    contentType: 'application/json',
-    dataType: 'json',
-    data: JSON.stringify({
-        task:{
-            content: 'Go to the beach'
+$('document').ready(function(){
+  function getAndDisplayAllTasks() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1137',
+        dataType: 'json',
+        success: function(response, textStatus) {
+            $('#todo-list').empty();
+            response.tasks.forEach(function (task) {
+                $('#todo-list').append('<p>' + task.content + '</p>');
+            })
+        },
+        error: function(request, testStatus, errorMessage) {
+            console.log(errorMessage);
         }
-    }),
-    succes: function (response, textStatus) {
-        console.log(response)
-    },
-    error: function (request, textStatus, errorMessage) {
+    });
+  }
+
+var createTask = function () {
+    $.ajax({
+      type: 'POST',
+      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1137',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: $('#new-task-content').val()
+        }
+      }),
+      success: function (response, textStatus) {
+        $('#new-task-content').val('');
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
         console.log(errorMessage);
-    }
-    
+      }
+    });
+  }
+  $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    createTask();
+  });
+  getAndDisplayAllTasks();  
 });
